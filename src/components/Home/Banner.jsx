a9f44f8ea3cc4a146c665f7fef701ddfd7675074
Mohammed_Assi <mohammad.assi@codehorizon.org>
2026-05-18 22:49:44 +0300

    fix banner in small screen

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -25,7 +25,7 @@ export const Banner = ({ allCategory, imagePath }) => {
   return (
     <div
       // dir={i18n.language === "ar" ? "rtl" : "ltr"}
-      className="relative md:flex text-center items-center justify-center lg:min-h-150 md:py-17 py-10 bg-gradient-to-r from-orange-100 to-blue-100 hidden"
+      className="relative flex flex-col md:flex-row text-center items-center justify-center lg:min-h-150 md:py-17 py-10 bg-gradient-to-r from-orange-100 to-blue-100"
     >
       {/* <div className="absolute bottom-0 start-0 hidden md:block">
         <img src={bannerLeft} alt="imageee" />
@@ -35,7 +35,7 @@ export const Banner = ({ allCategory, imagePath }) => {
         <img src={bannerRight} alt="imageee" className="object-contain" />
       </div> */}
 
-      <div className="w-150 relative z-10 px-3">
+      <div className="w-full max-w-150 relative z-10 px-4 md:px-3">
         <h1 className="font-bold text-2xl md:text-4xl lg:text-heading">
           {t("homeBannerHeading")}
         </h1>
@@ -64,21 +64,21 @@ export const Banner = ({ allCategory, imagePath }) => {
           {t("homeBannerSubheading")}
         </h3>
 
-        <div className="md:grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-6 mt-6 hidden">
+        <div className="flex flex-nowrap justify-center gap-4 md:gap-6 px-3 md:px-6 mt-6 overflow-x-auto">
           {allCategory?.map((category) => (
             <button
               onClick={() => {
                 navigate(`${ROUTE.PRODUCT_PAGE}?category=${category?.id}`);
               }}
               key={category.id}
-              className="flex flex-col items-center justify-center border bg-white border-gray-300 rounded-xl p-6 hover:shadow-lg hover:border-none transition group"
+              className="flex flex-col items-center justify-center shrink-0 w-20 md:w-auto md:flex-1 border bg-white border-gray-300 rounded-lg md:rounded-xl p-2.5 md:p-6 hover:shadow-lg hover:border-none transition group"
             >
               <img
                 src={`${imagePath}/${category?.image_url}`}
                 alt={category.name}
-                className="w-12 h-12 mb-3 group-hover:scale-110 transition"
+                className="w-8 h-8 md:w-12 md:h-12 mb-2 md:mb-3 group-hover:scale-110 transition"
               />
-              <p className="font-medium">{category.name}</p>
+              <p className="text-xs md:text-base font-medium">{category.name}</p>
             </button>
           ))}
         </div>
